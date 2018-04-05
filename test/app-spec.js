87e7d16e6c03614a8bdbe1e69e782af14e97c026
Uday Deshpande <udaydeshpande@Udays-MacBook-Pro.local>
2018-04-04 19:48:46 -0500

    first commit

---
+++ test/app-spec.js
@@ -0,0 +1,61 @@
+var request = require("supertest");
+var cheerio = require("cheerio");
+var expect = require('chai').expect;
+var rewire = require('rewire');
+var app = rewire('../app');
+
+
+describe("Dictionary App", function () {
+
+    it("Loads the home page", function(done){
+        request(app).get("/").expect(200).end(function(err, res){
+            var $ = cheerio.load(res.text);
+            var pageHeading = $("body>h1:first-child").text();
+            expect(pageHeading).equal("Skier Dictionary");
+            done();
+        });
+        
+    });
+
+    describe("Dictionary API", function () {
+
+        beforeEach(function () {    
+
+        	this.defs = [
+                {
+                    term: "One",
+                    defined: "Term One Defined"
+                },
+                {
+                    term: "Two",
+                    defined: "Term Two Defined"
+                }
+            ];
+
+            app.__set__("skierTerms", this.defs);
+        });
+
+        it("GETS dictionary-api", function(done){
+            var defs = this.defs;
+            request(app).get("/dictionary-api").expect(200).end(function(err, res){
+                var terms = JSON.parse(res.text);
+                expect(terms).to.deep.equal(defs);
+                done();
+            });
+        });
+
+        it("POSTS dictionary-api", function(done){
+            request(app)
+            .post("/dictionary-api")
+            .send({"term" : "Three", "defined" : "Term three defined"})
+            .expect(200)
+            .end(done);
+        });
+
+        it("DELETES dictionary-api", function(done){
+            request(app).delete("/dictionary-api/One").expect(200).end(done);
+        });
+
+    });
+
+});
